a06d2d04b45c2b8dd6f0c292b25f7d3b0200e800
Macrophylla (Bayu Arya) <bayuarya7570@gmail.com>
2025-06-01 17:30:07 +0700

    Add files via upload

---
+++ warkom.sql
@@ -0,0 +1,293 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 01, 2025 at 10:29 AM
+-- Server version: 8.4.3
+-- PHP Version: 8.3.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `warkom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_level`
+--
+
+CREATE TABLE `tb_level` (
+  `id_level` int NOT NULL,
+  `nama_level` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_level`
+--
+
+INSERT INTO `tb_level` (`id_level`, `nama_level`) VALUES
+(1, 'Administrator'),
+(2, 'Waiter'),
+(3, 'Kasir'),
+(4, 'Owner'),
+(5, 'Pelanggan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_masakan`
+--
+
+CREATE TABLE `tb_masakan` (
+  `id_masakan` int NOT NULL,
+  `nama_masakan` varchar(150) NOT NULL,
+  `harga` varchar(150) NOT NULL,
+  `stok` int NOT NULL,
+  `status_masakan` varchar(150) NOT NULL,
+  `gambar_masakan` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_masakan`
+--
+
+INSERT INTO `tb_masakan` (`id_masakan`, `nama_masakan`, `harga`, `stok`, `status_masakan`, `gambar_masakan`) VALUES
+(1, 'Nasi Ayam Goreng', '10000', 30, 'tersedia', 'nasiayamgoreng.png'),
+(2, 'Indomie', '7500', 100, 'tersedia', 'indomie.jpg'),
+(3, 'Nasi Ayam Geprek', '10000', 50, 'tersedia', 'nasiayamgeprek.jpg'),
+(4, 'Nasi Ayam Bakar', '10000', 60, 'tersedia', 'nasiayambakar.jpg'),
+(5, 'Es Jeruk', '5000', 100, 'tersedia', 'esjeruk.jpg'),
+(6, 'Es Teh', '3000', 100, 'tersedia', 'esteh.jpg'),
+(7, 'Lemon Tea', '5000', 100, 'tersedia', 'lemontea.jpg'),
+(8, 'Nasi Goreng', '10000', 78, 'tersedia', 'nasgor.jpg'),
+(9, 'Es Kopi', '5000', 200, 'tersedia', 'eskopi.jpg'),
+(10, 'Pop Ice All Varian', '5000', 100, 'tersedia', 'popice.jpg'),
+(11, 'Seblak', '10000', 40, 'tersedia', 'seblak.jpg'),
+(12, 'Air Putih + Es', '1000', 999, 'tersedia', 'airputihdingin.jpg'),
+(13, 'ES Dawet', '7000', 30, 'tersedia', 'Esdawet.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_order`
+--
+
+CREATE TABLE `tb_order` (
+  `id_order` int NOT NULL,
+  `id_admin` int DEFAULT NULL,
+  `id_pengunjung` int NOT NULL,
+  `waktu_pesan` datetime NOT NULL,
+  `no_meja` int NOT NULL,
+  `total_harga` varchar(150) NOT NULL,
+  `uang_bayar` varchar(150) DEFAULT NULL,
+  `uang_kembali` varchar(150) DEFAULT NULL,
+  `status_order` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_order`
+--
+
+INSERT INTO `tb_order` (`id_order`, `id_admin`, `id_pengunjung`, `waktu_pesan`, `no_meja`, `total_harga`, `uang_bayar`, `uang_kembali`, `status_order`) VALUES
+(14, 1, 9, '2019-08-03 12:43:52', 1, '48000', '50000', '2000', 'sudah bayar'),
+(27, 1, 15, '2025-05-30 18:44:42', 1, '20000', '50000', '30000', 'sudah bayar'),
+(28, 0, 1, '2025-05-30 18:45:55', 1, '1000', '', '', 'belum bayar');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pesan`
+--
+
+CREATE TABLE `tb_pesan` (
+  `id_pesan` int NOT NULL,
+  `id_user` int NOT NULL,
+  `id_order` int DEFAULT NULL,
+  `id_masakan` int NOT NULL,
+  `jumlah` int DEFAULT NULL,
+  `status_pesan` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_pesan`
+--
+
+INSERT INTO `tb_pesan` (`id_pesan`, `id_user`, `id_order`, `id_masakan`, `jumlah`, `status_pesan`) VALUES
+(64, 15, 27, 8, 2, 'sudah'),
+(65, 1, 28, 12, 1, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_stok`
+--
+
+CREATE TABLE `tb_stok` (
+  `id_stok` int NOT NULL,
+  `id_pesan` int NOT NULL,
+  `jumlah_terjual` int DEFAULT NULL,
+  `status_cetak` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_stok`
+--
+
+INSERT INTO `tb_stok` (`id_stok`, `id_pesan`, `jumlah_terjual`, `status_cetak`) VALUES
+(1, 46, 1, 'belum cetak'),
+(2, 47, 2, 'belum cetak'),
+(3, 48, 2, 'belum cetak'),
+(4, 49, 1, 'belum cetak'),
+(5, 50, 2, 'belum cetak'),
+(6, 51, 0, 'belum cetak'),
+(7, 52, 0, 'belum cetak'),
+(8, 53, 0, 'belum cetak'),
+(9, 54, 0, 'belum cetak'),
+(10, 55, 0, 'belum cetak'),
+(11, 56, 2, 'belum cetak'),
+(12, 57, 1, 'belum cetak'),
+(13, 58, 6, 'belum cetak'),
+(14, 59, 1, 'belum cetak'),
+(15, 60, 1, 'belum cetak'),
+(16, 61, 1, 'belum cetak'),
+(17, 62, 1, 'belum cetak'),
+(18, 63, 1, 'belum cetak'),
+(19, 64, 2, 'belum cetak'),
+(20, 65, 1, 'belum cetak');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` int NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `nama_user` varchar(150) NOT NULL,
+  `id_level` int NOT NULL,
+  `status` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_user`, `username`, `password`, `nama_user`, `id_level`, `status`) VALUES
+(1, 'admin', '123', 'Hendrik Setiawan', 1, 'aktif'),
+(6, 'hendro', '123', 'Hendro', 2, 'aktif'),
+(7, 'fitri', '123', 'Fitri', 3, 'aktif'),
+(8, 'slamet', '123', 'Slamet', 4, 'aktif'),
+(9, 'sugiastutik', '123', 'Sugiastutik', 4, 'aktif'),
+(15, 'andi', '123', 'Andi', 5, 'aktif');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_level`
+--
+ALTER TABLE `tb_level`
+  ADD PRIMARY KEY (`id_level`);
+
+--
+-- Indexes for table `tb_masakan`
+--
+ALTER TABLE `tb_masakan`
+  ADD PRIMARY KEY (`id_masakan`);
+
+--
+-- Indexes for table `tb_order`
+--
+ALTER TABLE `tb_order`
+  ADD PRIMARY KEY (`id_order`),
+  ADD KEY `id_admin` (`id_admin`),
+  ADD KEY `id_pengunjung` (`id_pengunjung`);
+
+--
+-- Indexes for table `tb_pesan`
+--
+ALTER TABLE `tb_pesan`
+  ADD PRIMARY KEY (`id_pesan`);
+
+--
+-- Indexes for table `tb_stok`
+--
+ALTER TABLE `tb_stok`
+  ADD PRIMARY KEY (`id_stok`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `id_level` (`id_level`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_level`
+--
+ALTER TABLE `tb_level`
+  MODIFY `id_level` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tb_masakan`
+--
+ALTER TABLE `tb_masakan`
+  MODIFY `id_masakan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `tb_order`
+--
+ALTER TABLE `tb_order`
+  MODIFY `id_order` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `tb_pesan`
+--
+ALTER TABLE `tb_pesan`
+  MODIFY `id_pesan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+
+--
+-- AUTO_INCREMENT for table `tb_stok`
+--
+ALTER TABLE `tb_stok`
+  MODIFY `id_stok` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD CONSTRAINT `tb_user_ibfk_1` FOREIGN KEY (`id_level`) REFERENCES `tb_level` (`id_level`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
